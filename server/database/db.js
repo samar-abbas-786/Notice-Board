5ceda1d7b2b127abe7d4cb1cf53b5d359b36518c
Samar Abbas <samarabbas172003@gmail.com>
2025-03-17 06:36:27 +0530

    Notice board 3

--- server/database/db.js
+++ server/database/db.js
@@ -1,7 +1,9 @@
 import mongoose from "mongoose";
-
+import dotenv from "dotenv";
+dotenv.config();
 export const db = () => {
-  mongoose.connect(process.env.MONGO_URI).then(() => {
-    console.log("DB connected");
-  });
+  mongoose
+    .connect(process.env.MONGO_URI)
+    .then(() => console.log("Connected to MongoDB"))
+    .catch((err) => console.error("MongoDB connection error:", err));
 };
